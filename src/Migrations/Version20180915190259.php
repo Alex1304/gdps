830a760c86cf77a38a3d214e8cfcbfa53bc418a6
Alex1304 <mirandaa1304@gmail.com>
2019-10-21 15:40:24 +0200

    Fixed a database migration file

--- src/Migrations/Version20180915190259.php
+++ src/Migrations/Version20180915190259.php
@@ -15,7 +15,7 @@ final class Version20180915190259 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE authorization (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, token VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7A6D8BEFA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE authorization (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, token VARCHAR(255) COLLATE utf8_unicode_ci  NOT NULL, UNIQUE INDEX UNIQ_7A6D8BEFA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE authorization ADD CONSTRAINT FK_7A6D8BEFA76ED395 FOREIGN KEY (user_id) REFERENCES account (id)');
     }
 
