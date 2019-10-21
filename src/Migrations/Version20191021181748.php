fb76974c55162d15b1af9bf2317780b3636d1a71
Alex1304 <mirandaa1304@gmail.com>
2019-10-22 01:03:43 +0200

    Removed insert chest statement

--- src/Migrations/Version20191021181748.php
+++ src/Migrations/Version20191021181748.php
@@ -23,7 +23,6 @@ final class Version20191021181748 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE chest (id INT AUTO_INCREMENT NOT NULL, cooldown INT NOT NULL, min_orbs INT NOT NULL, max_orbs INT NOT NULL, min_diamonds INT NOT NULL, max_diamonds INT NOT NULL, min_shards INT NOT NULL, max_shards INT NOT NULL, min_keys INT NOT NULL, max_keys INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-		$this->addSql("INSERT INTO chest VALUES (NULL, '14400', '20', '50', '1', '4', '0', '1', '5'), (NULL, '86400', '100', '300', '4', '10', '1', '2', '25');");
     }
 
     public function down(Schema $schema) : void
